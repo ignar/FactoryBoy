f0c60b51afd859ae928bb35992b0c3ae492423b7
Maxim Verbovoy <vermaxik@gmail.com>
2017-10-31 11:24:04 +0200

    Refactoring: added FactoryBuilder

--- lib/factory_boy.rb
+++ lib/factory_boy.rb
@@ -9,12 +9,23 @@ module FactoryBoy
   @factories = []
 
   def self.define_factory(klass_name)
-    @factories << klass_name
+    klass = FactoryBuilder.new(klass_name).build_to_class
+    @factories << klass
   end
 
   def self.build(klass_name)
-    raise "Factory wasn't defined" unless @factories.include?(klass_name)
-    klass = klass_name.is_a?(Class) ? klass_name : Object.const_get(klass_name.to_s.split('_').collect(&:capitalize).join)
+    klass = FactoryBuilder.new(klass_name).build_to_class
+    raise "Factory wasn't defined" unless @factories.include?(klass)
     klass.new
+  end
+end
+
+class FactoryBuilder
+  def initialize(klass_name)
+    @klass_name = klass_name
+  end
+
+  def build_to_class
+    @klass_name.is_a?(Class) ? @klass_name : Object.const_get(@klass_name.to_s.split('_').collect(&:capitalize).join)
   end
 end
