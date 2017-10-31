78e9258ed5ccebd6928178c8ba6d7e1324bbf6c7
Maxim Verbovoy <vermaxik@gmail.com>
2017-10-31 10:59:25 +0200

    Added expection if factory not defined

--- lib/factory_boy.rb
+++ lib/factory_boy.rb
@@ -1,12 +1,19 @@
 require "factory_boy/version"
 
+class User
+  attr_accessor :name, :email
+end
+
 module FactoryBoy
 
+  @factories = []
+
   def self.define_factory(klass_name)
-    @factory = klass_name
+    @factories << klass_name
   end
 
   def self.build(klass_name)
+    raise "Factory wasn't defined" unless @factories.include?(klass_name)
     klass_name.new
   end
 end
