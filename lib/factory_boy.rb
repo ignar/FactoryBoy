380e483a1bd0b4fe1396695bc1caa4e69ac67228
Maxim Verbovoy <vermaxik@gmail.com>
2017-10-31 12:42:48 +0200

    Passed instance variables through block

--- lib/factory_boy.rb
+++ lib/factory_boy.rb
@@ -8,31 +8,45 @@ module FactoryBoy
 
   @factories = {}
 
-  def self.define_factory(name, opts={})
-    klass = FactoryBuilder.new(opts.delete(:class) || name).build_to_class
+  def self.define_factory(name, opts={}, &block)
+    builder = FactoryBuilder.new(opts.delete(:class) || name, block)
     @attr = opts
-    @factories[name] = klass
+    @factories[name] = builder
   end
 
   def self.build(klass_name, attr={})
     raise FactoryNotDefinedError unless @factories.has_key?(klass_name)
-    object = @factories[klass_name].new
+    builder_object = @factories[klass_name]
+    object = builder_object.build_to_class.new
+
+    builder_object.set_instance(object)
     attributes = attr.empty? ? @attr : attr
     attributes.each do |key, value|
       object.send("#{key}=", value)
     end
+
     object
   end
 
 end
 
 class FactoryBuilder
-  def initialize(klass_name)
+  def initialize(klass_name, block)
     @klass_name = klass_name
+    @block = block
   end
 
   def build_to_class
     @klass_name.is_a?(Class) ? @klass_name : Object.const_get(@klass_name.to_s.split('_').collect(&:capitalize).join)
+  end
+
+  def set_instance(object)
+    @object = object
+    instance_eval(&@block) if @block
+  end
+
+  def method_missing(method, *args, &block)
+    @object.send("#{method}=", args.first)
   end
 end
 
