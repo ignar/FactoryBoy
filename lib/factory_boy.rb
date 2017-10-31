fa8983a166a626efaef75d16b7f79105f2cde121
Maxim Verbovoy <vermaxik@gmail.com>
2017-10-31 11:59:30 +0200

    Added ability to pass params to factory

--- lib/factory_boy.rb
+++ lib/factory_boy.rb
@@ -9,14 +9,21 @@ module FactoryBoy
   @factories = {}
 
   def self.define_factory(name, opts={})
-    klass = FactoryBuilder.new(opts[:class] || name).build_to_class
+    klass = FactoryBuilder.new(opts.delete(:class) || name).build_to_class
+    @attr = opts
     @factories[name] = klass
   end
 
-  def self.build(klass_name)
-    raise "Factory wasn't defined" unless @factories.has_key?(klass_name)
-    @factories[klass_name].new
+  def self.build(klass_name, attr={})
+    raise FactoryNotDefinedError unless @factories.has_key?(klass_name)
+    object = @factories[klass_name].new
+    attributes = attr.empty? ? @attr : attr
+    attributes.each do |key, value|
+      object.send("#{key}=", value)
+    end
+    object
   end
+
 end
 
 class FactoryBuilder
@@ -28,3 +35,5 @@ class FactoryBuilder
     @klass_name.is_a?(Class) ? @klass_name : Object.const_get(@klass_name.to_s.split('_').collect(&:capitalize).join)
   end
 end
+
+class FactoryNotDefinedError < StandardError; end
